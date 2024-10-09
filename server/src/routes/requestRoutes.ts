5f7b6992500f1df4b14db2009e4795a845cb1c1b
Nipuna Kaweya <nipuna609@gmail.com>
2024-10-09 19:47:40 +0530

    Routes file added

---
+++ server/src/routes/requestRoutes.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import {
+    getAllRequests,
+    getRequestById,
+    createRequest,
+    updateRequest,
+    deleteRequest
+} from '../controllers/requestController';
+
+const router = express.Router();
+
+// Get all request
+router.get('/',getAllRequests);
+
+// Get a single request by ID
+router.get('/:id',getRequestById as any)
+
+// Post a request
+router.post('/',createRequest);
+
+// Update a request by id
+router.patch('/:id',updateRequest as any);
+// Delete a request by id
+router.delete('/:id',deleteRequest as any)
