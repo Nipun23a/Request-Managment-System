969b138f559095908e08ef72a5b98213085c0375
Nipuna Kaweya <nipuna609@gmail.com>
2024-10-10 15:58:52 +0530

    Icon atoms updated

--- client/src/components/atoms/Icon.tsx
+++ client/src/components/atoms/Icon.tsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React from 'react';
+import { IconType } from 'react-icons';
+import { IoHomeOutline } from "react-icons/io5";
+import { BiMessageDetail } from "react-icons/bi";
+import { CgNotes } from "react-icons/cg";
+import { FiUser } from "react-icons/fi";
+import { FaHome, FaList, FaComments, FaChartBar, FaUser, FaCog, FaChevronDown, FaMoon, FaBell,FaPlus } from 'react-icons/fa';
 
-interface IconProps{
-    name: string;
-    className?: string;
+interface IconProps {
+  name: string;
+  className?: string;
 }
 
-export const Icon: React.FC<IconProps> = ({ name, className = '' }) => (
-    <i className={`fas fa-${name} ${className}`}></i>
-  );
+const icons: { [key: string]: IconType } = {
+  'tachometer-alt': IoHomeOutline,
+  'list': BiMessageDetail,
+  'comments': FaComments,
+  'chart-bar': FaChartBar,
+  'user': FiUser,
+  'cog': FaCog,
+  'chevron-down': FaChevronDown,
+  'moon': FaMoon,
+  'bell': FaBell,
+  'plus': FaPlus,
+};
+
+export const Icon: React.FC<IconProps> = ({ name, className = '' }) => {
+  const IconComponent = icons[name];
+
+  if (!IconComponent) {
+    console.warn(`Icon "${name}" not found`);
+    return null;
+  }
+
+  return <IconComponent className={`w-6 h-6 ${className}`} />;
+};
