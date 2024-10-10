554773cc37bdb4499e65f0ba53ff4f3ee168bdf3
Nipuna Kaweya <nipuna609@gmail.com>
2024-10-10 15:59:31 +0530

    Layout updated

--- client/src/components/templates/Layout.tsx
+++ client/src/components/templates/Layout.tsx
@@ -9,7 +9,7 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => (
   <div className="min-h-screen flex flex-col">
     <Header />
     <main className="flex-grow bg-gray-100">
-      <div className="container mx-auto px-4 py-8">
+      <div className="container mx-auto px-4 py-6">
         <div className="max-w-7xl mx-auto">
           {children}
         </div>
