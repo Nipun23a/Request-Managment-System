c38c9df71f9cf54acd35425916621b69e8aa5acf
Nipuna Kaweya <nipuna609@gmail.com>
2024-10-10 15:33:28 +0530

    Request Page is Updated

---
+++ client/src/components/pages/RequstsPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { RequestSummary } from '../organisms/RequestSummary';
+
+export const RequestsPage: React.FC = () => (
+  <div>
+    <RequestSummary />
+  </div>
+);
