b68efc7c8251a51dee2e10eddda9acda17cda70a
Nipuna Kaweya <nipuna609@gmail.com>
2024-10-10 15:56:47 +0530

    button updated

---
+++ client/src/components/atoms/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface ButtonProps {
+  children: React.ReactNode;
+  onClick?: () => void;
+  className?: string;
+}
+
+export const Button: React.FC<ButtonProps> = ({ children, onClick, className = '' }) => (
+  <button onClick={onClick} className={`px-4 py-2 rounded ${className}`}>
+    {children}
+  </button>
+);
