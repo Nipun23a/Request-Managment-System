7ccde9ba0e0a1ecd69b4af96aa19511274f8c6fe
Nipuna Kaweya <nipuna609@gmail.com>
2024-10-10 15:59:20 +0530

    Request Summary Updated

--- client/src/components/organisms/RequestSummary.tsx
+++ client/src/components/organisms/RequestSummary.tsx
@@ -4,12 +4,12 @@ import { StatusCircle } from '../molecules/StatusCircle';
 import { NewRequestButton } from '../molecules/NewRequestButton';
 
 export const RequestSummary: React.FC = () => (
-  <div className='main flex flex-col lg:flex-row justify-between items-center p-4 space-y-4 lg:space-y-0'>
-    <div className="flex flex-col sm:flex-row items-start sm:items-center space-y-4 sm:space-y-0 sm:space-x-4 lg:w-auto">
+  <div className='main flex flex-col lg:flex-row justify-between items-center p-4 space-y-6 lg:space-y-0 lg:space-x-8'>
+    <div className="flex flex-col sm:flex-row items-center sm:items-center space-y-4 sm:space-y-0 sm:space-x-4 lg:w-auto">
       <Text className="text-2xl font-bold navigation whitespace-nowrap">Requests</Text>
       <NewRequestButton />
     </div>
-    <div className='circle-section flex flex-wrap justify-center lg:justify-end gap-4 lg:ml-8'>
+    <div className='circle-section flex flex-wrap justify-center lg:justify-end gap-4'>
       <StatusCircle count={10} label="New Requests" color="bg-pink-200" />
       <StatusCircle count={5} label="Delayed Requests" color="bg-green-200" />
       <StatusCircle count={2} label="Escalated Requests" color="bg-blue-200" />
