92cc18b21da60f1e60f5be89c9736c68fc99f8b0
Nipuna Kaweya <nipuna609@gmail.com>
2024-10-10 15:56:30 +0530

    Updated App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Header } from './components/organisms/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Layout } from './components/templates/Layout';
+import { RequestsPage } from './components/pages/RequstsPage';
 
+// Import other pages...
 
-const App: React.FC = () => {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        {/* Your other components and routes go here */}
-      </div>
-    </Router>
-  );
-};
+const App: React.FC = () => (
+  <Router>
+    <Layout>
+      <Routes>
+        <Route path="/requests" element={<RequestsPage />} />
+        {/* Add other routes... */}
+      </Routes>
+    </Layout>
+  </Router>
+);
 
 export default App;
