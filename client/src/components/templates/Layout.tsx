ea1cffdb7abd942a70d78f0abacfbdb04ffbf80f
Nipuna Kaweya <nipuna609@gmail.com>
2024-10-12 11:04:39 +0530

    ff

--- client/src/components/templates/Layout.tsx
+++ client/src/components/templates/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState,  useCallback } from 'react';
 import axios from 'axios';
 import { Header } from '../organisms/Header';
 import { RequestSummary } from '../organisms/RequestSummary';
